f80b5afe9b525d1876c8e88222bf4b833537d1de
brianush1 <brianush1@outlook.com>
2024-02-29 11:26:24 -0500

    add vec2 class

---
+++ src/math.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <cmath>
+
+struct vec2 {
+	double x, y;
+
+	vec2(double s = 0) : x(s), y(s) {}
+	vec2(double x, double y) : x(x), y(y) {}
+
+	vec2 operator+(const vec2 &other) const { return vec2(x + other.x, y + other.y); }
+	vec2 operator-(const vec2 &other) const { return vec2(x - other.x, y - other.y); }
+	vec2 operator*(const vec2 &other) const { return vec2(x * other.x, y * other.y); }
+	vec2 operator/(const vec2 &other) const { return vec2(x / other.x, y / other.y); }
+
+	double dot(const vec2 &other) const {
+		return x * other.x + y * other.y;
+	}
+
+	double cross(const vec2 &other) const {
+		return x * other.y - y * other.x;
+	}
+
+	double length() const {
+		return std::sqrt(dot(*this));
+	}
+
+	bool operator==(const vec2 &other) const {
+		return x == other.x && y == other.y;
+	}
+
+	bool operator!=(const vec2 &other) const {
+		return !(*this == other);
+	}
+
+};
